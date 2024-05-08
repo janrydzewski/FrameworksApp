c4246afda31356ddde724d5f316cb6d8d899f730
j_rydzewski <52713164+janrydzewski@users.noreply.github.com>
2024-05-08 13:26:21 +0200

    Initial Commit

---
+++ FrameworksApp/FrameworksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksAppApp.swift
+//  FrameworksApp
+//
+//  Created by Jan Rydzewski on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
